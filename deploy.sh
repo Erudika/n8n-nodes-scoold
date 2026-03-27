0c2c29ed6f2fae51bdbf8d83c248df33a95b9040
Alex Bogdanovski <alex@erudika.com>
2026-03-27 10:14:28 +0200

    prepare release

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-npm run build && \
-git add -A && git commit -am "prepare release" && \
-npm run release
+echo "Last tag was:" $(git describe --tags --abbrev=0)
+read -e -p "(p)atch or (m)inor? P/m " ver
+git add -A && git commit -m "prepare release"
+if [[ "$ver" = "m" ]]; then
+	npm version minor
+else
+	npm version patch
+fi
+git push origin main && git push --tags
+
+
+
+#npm run build && \
+#git add -A && git commit -am "prepare release"
+#npm run release
 
